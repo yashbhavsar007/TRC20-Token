19c3779b74d645e75c9472b78c982dff49e683fb
Yash Bhavsar <yashbhavsar.yb@gmail.com>
2019-01-24 18:03:22 +0530

    Update tronbox.js

--- tronbox.js
+++ tronbox.js
@@ -1,8 +1,8 @@
 module.exports = {
     networks: {
         development: {
-            from: 'TR7Kg2Yf5swVFm2hbC6GX9MQzVzuA1RwV3',
-            privateKey: '431996138f9c8592c9e632213b05b84d53afe831ba36d989a2a871ad214299d3',
+            from: 'YOUR_PUBLIC_KEY',
+            privateKey: 'YOUR_PRIVATE_KEY',
             userFeePercentage: 30, // or consume_user_resource_percent
             feeLimit: 100000000, // or fee_limit
            // originEnergyLimit: 10000000, // or origin_energy_limit
